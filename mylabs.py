b54b69d8d63c19568698402614cb6835081e90b0
Yaf73 <yanick.favre@orange.fr>
2019-04-27 14:04:08 +0200

    Test compilateur
    Essai du compilateur pylint

--- mylabs.py
+++ mylabs.py
@@ -1,5 +1,6 @@
 # -*- coding: iso-8859-1 -*-
 
+
 """
 ---------------------------------------------------------------------
 Application Mylabs
@@ -14,23 +15,25 @@ Version:              1.0.0
 """
 
 import sys
-from PyQt5 import QApplication, QMainWindow
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QIcon
 
 class Mylabs(QMainWindow):
 
     def __init__(self):
-        super.__init__()
-        self.initUI()
+        # Initialisation de la fenêtre
+        super().__init__()
+        self.initui()
 
-    def initUI(self):
+    def initui(self):
         # Paramétrage des données principale de la fenêtre
-        self.setWidowTitle("Mylabs")
+        self.setWindowTitle("Mylabs")
         self.setGeometry(8, 30, 800, 600)
         self.setWindowIcon(QIcon("images/histo.png"))
 
 if __name__ == "__main__":
-    app = QApplication()
+    app = QApplication(sys.argv)
     mylabs = Mylabs()
     sys.exit(app.exec_())
 
